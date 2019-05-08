4aa1f32b3f16877f7982d078de89cfece8e2aff0
diegobeltran97 <diegoelcairo@gmail.com>
2019-05-07 20:05:17 -0500

    test2

---
+++ docs/CHARITY – Just another WordPress site_files/custom.js
@@ -0,0 +1,140 @@
+jQuery(document).ready( function(){
+	var ww = jQuery(window).width();	
+	
+	if( ww > 479 && ww < 719){
+		jQuery('.client').removeClass('last');
+	}
+
+	jQuery("area[rel^='prettyPhoto']").prettyPhoto();
+	jQuery(".gallery:first a[rel^='prettyPhoto']").prettyPhoto({animation_speed:'normal',theme:'light_square',slideshow:3000, autoplay_slideshow: false});
+	jQuery(".gallery:gt(0) a[rel^='prettyPhoto']").prettyPhoto({animation_speed:'fast',slideshow:10000, hideflash: true});
+	jQuery("#custom_content a[rel^='prettyPhoto']:first").prettyPhoto({
+		custom_markup: '<div id="map_canvas" style="width:260px; height:265px"></div>',
+		changepicturecallback: function(){ initialize(); }
+	});
+	jQuery("#custom_content a[rel^='prettyPhoto']:last").prettyPhoto({
+		custom_markup: '<div id="bsap_1259344" class="bsarocks bsap_d49a0984d0f377271ccbf01a33f2b6d6"></div><div id="bsap_1237859" class="bsarocks bsap_d49a0984d0f377271ccbf01a33f2b6d6" style="height:260px"></div><div id="bsap_1251710" class="bsarocks bsap_d49a0984d0f377271ccbf01a33f2b6d6"></div>',
+		changepicturecallback: function(){ _bsap.exec(); }
+	});
+
+});
+
+jQuery(document).ready(function() { 
+   jQuery('#testimonials .quotes').quovolver({
+      children    : 'li',
+      transitionSpeed : 600,
+      autoPlay    : true,
+	  autoPlaySpeed:6000,
+      equalHeight   : false,
+      navPosition   : 'below',
+      navPrev     : true,
+      navNext     : true,
+      navNum      : true,
+      navText     : false,
+      navTextContent  : 'Quote @a of @b'
+    });
+    
+  });
+
+
+// NAVIGATION CALLBACK
+var ww = jQuery(window).width();
+jQuery(document).ready(function() { 
+	jQuery(".nav li a").each(function() {
+		if (jQuery(this).next().length > 0) {
+			jQuery(this).addClass("parent");
+		};
+	})
+	jQuery(".toggleMenu").click(function(e) { 
+		e.preventDefault();
+		jQuery(this).toggleClass("active");
+		jQuery(".nav").slideToggle('fast');
+	});
+	adjustMenu();
+})
+
+// navigation orientation resize callbak
+jQuery(window).bind('resize orientationchange', function() {
+	ww = jQuery(window).width();
+	adjustMenu();
+});
+
+var adjustMenu = function() {
+	if (ww < 981) {
+		jQuery(".toggleMenu").css("display", "block");
+		if (!jQuery(".toggleMenu").hasClass("active")) {
+			jQuery(".nav").hide();
+		} else {
+			jQuery(".nav").show();
+		}
+		jQuery(".nav li").unbind('mouseenter mouseleave');
+	} else {
+		jQuery(".toggleMenu").css("display", "none");
+		jQuery(".nav").show();
+		jQuery(".nav li").removeClass("hover");
+		jQuery(".nav li a").unbind('click');
+		jQuery(".nav li").unbind('mouseenter mouseleave').bind('mouseenter mouseleave', function() {
+			jQuery(this).toggleClass('hover');
+		});
+	}
+}
+
+	jQuery(document).ready(function() {
+  	jQuery('.srchicon').click(function() {
+			jQuery('.searchtop').toggle();
+			jQuery('.topsocial').toggle();
+		});	
+});
+	
+jQuery(document).ready(function(){
+            var submitIcon = jQuery('.searchbox-icon');
+            var inputBox = jQuery('.searchbox-input');
+            var searchBox = jQuery('.searchbox');
+            var isOpen = false;
+            submitIcon.click(function(){
+                if(isOpen == false){
+                    searchBox.addClass('searchbox-open');
+                    inputBox.focus();
+                    isOpen = true;
+                } else {
+                    searchBox.removeClass('searchbox-open');
+                    inputBox.focusout();
+                    isOpen = false;
+                }
+            });  
+             submitIcon.mouseup(function(){
+                    return false;
+                });
+            searchBox.mouseup(function(){
+                    return false;
+                });
+            jQuery(document).mouseup(function(){
+                    if(isOpen == true){
+                        jQuery('.searchbox-icon').css('display','block');
+                        submitIcon.click();
+                    }
+                });
+        });
+            function buttonUp(){
+                var inputVal = jQuery('.searchbox-input').val();
+                inputVal = jQuery.trim(inputVal).length;
+                if( inputVal !== 0){
+                    jQuery('.searchbox-icon').css('display','none');
+                } else {
+                    jQuery('.searchbox-input').val('');
+                    jQuery('.searchbox-icon').css('display','block');
+                }
+}
+
+jQuery(document).ready(function() {
+        jQuery('h2.section_title, .sponsercontent h2, .needyourhelp-content h1').each(function(index, element) {
+            var heading = jQuery(element);
+            var word_array, last_word, first_part;
+
+            word_array = heading.html().split(/\s+/); // split on spaces
+            last_word = word_array.pop();             // pop the last word
+            first_part = word_array.join(' ');        // rejoin the first words together
+
+            heading.html([first_part, ' <span>', last_word, '</span>'].join(''));
+        });
+});
